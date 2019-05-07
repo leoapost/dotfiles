f8b2ab4bc4e6a5d7556179fe9dea80cfcc463661
Leonidas Apostolidis <leoapost@gmail.com>
2019-05-07 15:52:41 +0100

    Update brew packages

--- brew.sh
+++ brew.sh
@@ -2,11 +2,24 @@
 
 # Install command-line tools using Homebrew.
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade
+brew upgrade --all
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -28,8 +41,8 @@ brew install bash-completion2
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
 fi;
+chsh -s /usr/local/bin/bash;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -57,6 +70,7 @@ brew install dns2tcp
 brew install fcrackzip
 brew install foremost
 brew install hashpump
+brew install hub
 brew install hydra
 brew install john
 brew install knock
@@ -73,35 +87,33 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
+brew install aria2
+brew install ccat
 brew install dark-mode
 brew install git
 brew install git-lfs
+brew install httpie
 brew install imagemagick --with-webp
 brew install lynx
+brew install noti
+brew install nvm
 brew install p7zip
+brew install peco
 brew install pigz
 brew install pv
 brew install rename
+brew install siege
 brew install speedtest_cli
 brew install ssh-copy-id
 brew install testssl
+brew install the_silver_searcher
 brew install tree
 brew install vbindiff
+brew install watch
 brew install webkit2png
-brew install zopfli
-brew install the_silver_searcher
-brew install ccat
-brew install httpie
-brew install siege
 brew install wrk
 brew install yajl
-brew install watch
-brew install peco
-brew install docker
-brew install docker-machine
-brew install nvm
-brew install aria2
-brew install noti
+brew install zopfli
 
 # Install Caskroom
 brew tap caskroom/cask
@@ -109,47 +121,50 @@ brew install brew-cask
 brew tap caskroom/versions
 
 # Install Cask packages
-apps=(
-    dropbox
-    evernote
-    firefox
-    flux
-    google-chrome
-    google-chrome-canary
-    google-drive
-    iterm2
-    ngrok
-    skype
-    slack
-    spotify
-    sublime-text3
-    transmit
-    virtualbox
-    wireshark
-    rdiff-backup
-    cyberduck
-    gitify
-    karabiner-elements
-)
-brew cask install "${apps[@]}"
-
+brew cask install cyberduck
+brew cask install dash
+brew cask install docker
+brew cask install dropbox
+brew cask install evernote
+brew cask install firefox
+brew cask install flux
+brew cask install gitify
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install google-drive
+brew cask install iterm2
+brew cask install karabiner-elements
+brew cask install keybase
+brew cask install meld
+brew cask install ngrok
+brew cask install pgadmin4
+brew cask install postman
+brew cask install rdiff-backup
+brew cask install skype
+brew cask install skype
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text3
+brew cask install telegram
+brew cask install transmission
+brew cask install transmit
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install wireshark
 
 brew tap homebrew/services
 
 # Install Quick Look plugins
-quick_look_plugins=(
-  qlcolorcode
-  qlstephen
-  qlmarkdown
-  quicklook-json
-  qlprettypatch
-  quicklook-csv
-  betterzipql
-  qlimagesize
-  webpquicklook
-  suspicious-package
- )
-brew cask install "${quick_look_plugins[@]}"
+brew cask install  qlcolorcode
+brew cask install  qlstephen
+brew cask install  qlmarkdown
+brew cask install  quicklook-json
+brew cask install  qlprettypatch
+brew cask install  quicklook-csv
+brew cask install  betterzipql
+brew cask install  qlimagesize
+brew cask install  webpquicklook
+brew cask install  suspicious-package
 
 # Remove outdated versions from the cellar.
 brew cleanup
